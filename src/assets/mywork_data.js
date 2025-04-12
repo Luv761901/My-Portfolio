a05b0143b9b7d9f0102485733ca10eb1d20c4b1c
Luv761901 <aluv49295@gmail.com>
2025-04-12 21:22:02 +0530

    feat: mywork images

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,4 +1,4 @@
-import project1_img from '../assets/project_1.svg'
+import project1_img from '../assets/website.jpg'
 import project2_img from '../assets/project_2.svg'
 import project3_img from '../assets/project_3.svg'
 import project4_img from '../assets/project_4.svg'
@@ -9,32 +9,38 @@ const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:project1_img,
+        link:"https://zenstreet.ai/"
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:project2_img,
+        link:"https://zenstreet.ai/"
     },
     {
         w_no:3,
         w_name:"Web design",
-        w_img:project3_img
+        w_img:project3_img,
+        link:"https://zenstreet.ai/"
     },
     {
         w_no:4,
         w_name:"Web design",
-        w_img:project4_img
+        w_img:project4_img,
+        link:"https://zenstreet.ai/"
     },
     {
         w_no:5,
         w_name:"Web design",
-        w_img:project5_img
+        w_img:project5_img,
+        link:"https://zenstreet.ai/"
     },
     {
         w_no:6,
         w_name:"Web design",
-        w_img:project6_img
+        w_img:project6_img,
+        link:"https://zenstreet.ai/"
     },
 ]
  
